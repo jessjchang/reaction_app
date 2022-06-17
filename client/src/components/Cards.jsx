39ac62d08c81a07fc3eb6806496478cfadbac07f
Andrew Moore <andrewdmoore84@gmail.com>
2022-06-17 12:16:00 -0400

    Move add-a-card link to List component
    Co-authored-by: Jessica Chang <jesschang77@gmail.com>

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -9,24 +9,6 @@ const Cards = ({ listId }) => {
   return (
     <div id="cards-container" data-id="list-1-cards">
       {currentCards.map(card => <Card key={card._id} cardInfo={card} />)}
-
-      <div className="add-dropdown add-bottom">
-        <div className="card">
-          <div className="card-info"></div>
-          <textarea name="add-card"></textarea>
-          <div className="members"></div>
-        </div>
-        <a className="button">Add</a>
-        <i className="x-icon icon"></i>
-        <div className="add-options">
-          <span>...</span>
-        </div>
-      </div>
-
-      <div className="add-card-toggle" data-position="bottom">
-        Add a card...
-      </div>
-
     </div>
   );
 };
--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -12,7 +12,24 @@ const List = ({ listInfo }) => {
           </div>
           <Cards listId={listInfo._id} />
 
-          {/* <div className="add-dropdown add-top">
+          <div className="add-dropdown add-bottom">
+            <div className="card">
+              <div className="card-info"></div>
+              <textarea name="add-card"></textarea>
+              <div className="members"></div>
+            </div>
+            <a className="button">Add</a>
+            <i className="x-icon icon"></i>
+            <div className="add-options">
+              <span>...</span>
+            </div>
+          </div>
+
+          <div className="add-card-toggle" data-position="bottom">
+            Add a card...
+          </div>
+{/*
+          <div className="add-dropdown add-top">
             <div className="card"></div>
             <a className="button">Add</a>
             <i className="x-icon icon"></i>
