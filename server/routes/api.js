d8f9e46b76b658b3def24b23b83d145758d2a22b
Jessica Chang <jesschang77@gmail.com>
2022-06-20 14:23:03 -0400

    Implement card creation endpoint
    Co-authored-by: Andrew Moore <andrewdmoore84@gmail.com>

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const router = express.Router();
 const boardsController = require("../controllers/boardsController");
 const listsController = require("../controllers/listsController");
+const cardsController = require("../controllers/cardsController");
 const { validateBoard, validateList } = require("../validators/validators");
 
 router.get('/boards',boardsController.getBoards );
@@ -13,5 +14,9 @@ router.post('/boards', validateBoard, boardsController.createBoard );
 router.post('/lists', validateList, listsController.createList );
 
 router.put('/lists/:id', listsController.editList);
+
+router.get('/cards/:id', cardsController.getCard);
+
+router.post('/cards', cardsController.createCard);
 
 module.exports = router;
